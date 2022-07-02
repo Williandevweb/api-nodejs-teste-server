6808ea22697404d09a82c5686110426a6356d4b4
Willian - Desenvolvedor <80816594+Williandevweb@users.noreply.github.com>
2022-07-02 08:46:19 -0300

    Update index.js
    ALTERANDO DESCRIÇÃO

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(request,response)=>{
-    return response.json({message:'Server funcionando status 200 alterando para teste'});
+    return response.json({message:'Server funcionando status 200 INTEGRAÇÃO PCF PEÇAS E SERVIÇOS PARA TRATORES'});
 })
 
 app.listen(4000)
