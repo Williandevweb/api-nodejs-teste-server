68e43d948daf372f6da9db33eea1dae7d986e585
Willian - Desenvolvedor <80816594+Williandevweb@users.noreply.github.com>
2022-07-02 00:00:40 -0300

    Update index.js
    Alterado para teste

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(request,response)=>{
-    return response.json({message:'Server funcionando status 200'});
+    return response.json({message:'Server funcionando status 200 alterando para teste'});
 })
 
 app.listen(4000)
